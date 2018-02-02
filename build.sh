27946e32568d4808bd1b402609e46dd8728cd54a
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-02-02 16:31:53 +0300

    Initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -xe
+
+# We need --release since debug version at the moment doesn't work properly.
+RUSTFLAGS="-g" cargo build --release --target wasm32-unknown-unknown
+
+command -v wasm2wat || ( echo "wasm2wat isn't installed"; exit 1 )
+ ~/dev/etc/wabt/wasm2wat -f target/wasm32-unknown-unknown/release/closure_alloc.wasm > closure_alloc.wast
+ 
