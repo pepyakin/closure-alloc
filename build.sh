b321d4a4de282cb17f7aa0c326a28cf664e37d3a
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-02-02 16:38:01 +0300

    Get size estimate

--- build.sh
+++ build.sh
@@ -6,5 +6,9 @@ set -xe
 RUSTFLAGS="-g" cargo build --release --target wasm32-unknown-unknown
 
 command -v wasm2wat || ( echo "wasm2wat isn't installed"; exit 1 )
- ~/dev/etc/wabt/wasm2wat -f target/wasm32-unknown-unknown/release/closure_alloc.wasm > closure_alloc.wast
+ ~/dev/etc/wabt/wasm2wat \
+    -f \
+    target/wasm32-unknown-unknown/release/closure_alloc.wasm > closure_alloc.wast
  
+# To get estimate of size
+~/dev/etc/wabt/wasm2wat target/wasm32-unknown-unknown/release/closure_alloc.wasm | wc -l
